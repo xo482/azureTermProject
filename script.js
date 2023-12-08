91a6de98a5f1563173296c63637ff25db0aa00bf
xo482 <ksh990409@naver.com>
2023-12-08 18:17:14 +0900

    test

---
+++ script.js
@@ -0,0 +1,34 @@
+// script.js
+
+// 함수 엔드포인트 URL
+const functionUrl = "https://testfunctionxx.azurewebsites.net/api/HttpTrigger1?code=ySDrPhUUD1JNTbMyEN0d6qLFiD5cjZpsp_nezJ2dNxXKAzFu9UozCQ=="; // Azure Functions의 URL로 대체
+
+// 클릭 이벤트 핸들러
+async function vote(id) {
+    try {
+        const response = await fetch(`${functionUrl}/api/vote?id=${id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            console.error('Failed to vote:', response.statusText);
+            return;
+        }
+
+        const result = await response.json();
+        console.log(result);
+
+        // 여기에서 투표 결과에 따른 UI 업데이트 로직을 추가할 수 있습니다.
+
+    } catch (error) {
+        console.error('Error while voting:', error);
+    }
+}
+
+// 각 버튼에 이벤트 리스너 등록
+document.getElementById('button1').addEventListener('click', () => vote(1));
+document.getElementById('button2').addEventListener('click', () => vote(2));
+document.getElementById('button3').addEventListener('click', () => vote(3));
